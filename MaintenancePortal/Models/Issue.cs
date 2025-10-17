cdbf24ce6296caa323de3f51fa6b83b5e991ddd6
CW-ISM <cwismgitism@gmail.com>
2025-10-16 20:18:11 -0700

    Add `Issue` model to represent issue tracking data
    A new `Issue` class was introduced in the `MaintenancePortal.Models` namespace. This class includes the following required properties: `Gene`, `Depth`, `Order`, `Title`, `Description`, `CreatedAt`, and `CreatedByUserId`. The `CreatedByUserId` property is marked with the `[ForeignKey("User")]` attribute to establish a foreign key relationship with the `User` entity.
    
    Additionally, a computed property `DisplayId` was added, which formats `Gene`, `Depth`, and `Order` into a string representation. This property is excluded from database mapping using the `[NotMapped]` attribute.
    
    The `System.ComponentModel.DataAnnotations.Schema` namespace was imported to support the use of `[ForeignKey]` and `[NotMapped]` attributes.

---
+++ MaintenancePortal/Models/Issue.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MaintenancePortal.Models;
+
+public class Issue
+{
+    public required long Gene { get; set; }
+    public required short Depth { get; set; }
+    public required short Order { get; set; }
+    public required string Title { get; set; }
+    public required string Description { get; set; }
+    public required DateTime CreatedAt { get; set; }
+    [ForeignKey("User")]
+    public required string CreatedByUserId { get; set; }
+
+    [NotMapped]
+    public string DisplayId => $"{Gene:D8}-{Depth:D3}-{Order:D3}";
+}
