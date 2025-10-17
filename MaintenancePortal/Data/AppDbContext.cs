4cc806ccdd088d9e6fb5c63c10158a8364373d4b
CW-ISM <cwismgitism@gmail.com>
2025-10-16 20:14:48 -0700

    Add AppDbContext with Identity support
    Introduced the `AppDbContext` class in the `MaintenancePortal.Data` namespace, inheriting from `IdentityDbContext<User>` to enable ASP.NET Core Identity with a custom `User` model.
    
    Added necessary `using` directives for `MaintenancePortal.Models`, `Microsoft.AspNetCore.Identity.EntityFrameworkCore`, and `Microsoft.EntityFrameworkCore`. Implemented a constructor to configure the `AppDbContext` with `DbContextOptions<AppDbContext>`.

---
+++ MaintenancePortal/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using MaintenancePortal.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MaintenancePortal.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options)
+     : IdentityDbContext<User>(options)
+{
+    
+}
