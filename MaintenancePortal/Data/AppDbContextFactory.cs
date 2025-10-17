0120c0299c1cbc04b5a93f540d2232be8be67011
CW-ISM <cwismgitism@gmail.com>
2025-10-16 20:15:19 -0700

    Add AppDbContextFactory for design-time DbContext creation
    Introduced the `AppDbContextFactory` class to support design-time
    creation of `AppDbContext`, enabling migrations and other tooling.
    Added necessary `using` directives for Entity Framework Core and
    organized the code under the `MaintenancePortal.Data` namespace.
    Configured the `CreateDbContext` method to use SQL Server as the
    database provider.

---
+++ MaintenancePortal/Data/AppDbContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace MaintenancePortal.Data;
+
+public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+        optionsBuilder.UseSqlServer();
+
+        return new AppDbContext(optionsBuilder.Options);
+    }
+}
