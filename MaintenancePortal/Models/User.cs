c056fe2aa75b41de44d0dda478cc77f49cd9b6a9
CW-ISM <cwismgitism@gmail.com>
2025-10-16 20:17:20 -0700

    Add custom User class with additional required fields
    Created a `User` class in the `MaintenancePortal.Models` namespace, inheriting from `IdentityUser`.
    Added required properties `FirstName`, `LastName`, and `BirthDate` to store user-specific information.
    Included the `using Microsoft.AspNetCore.Identity;` directive to support the `IdentityUser` base class.

---
+++ MaintenancePortal/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MaintenancePortal.Models;
+
+public class User : IdentityUser
+{
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required DateTime BirthDate { get; set; }
+
+}
