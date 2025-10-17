bafc6a953b1cd3588c3d6b4bfbb6a37606954ab2
CW-ISM <cwismgitism@gmail.com>
2025-10-16 23:42:30 -0700

    Remove unused System.Reflection.Emit directive
    The `using System.Reflection.Emit;` directive was removed from the `IssueConfiguration.cs` file as it is no longer required. This cleanup improves code readability and eliminates unnecessary dependencies.

--- MaintenancePortal/Configurations/IssueConfiguration.cs
+++ MaintenancePortal/Configurations/IssueConfiguration.cs
@@ -1,7 +1,6 @@
 ﻿using MaintenancePortal.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System.Reflection.Emit;
 
 namespace MaintenancePortal.Configurations;
 
