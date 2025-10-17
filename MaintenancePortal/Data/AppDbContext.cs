d32fb477ddb6f96f1bb4ea79c0647bba4c029cb3
CW-ISM <cwismgitism@gmail.com>
2025-10-16 23:28:19 -0700

    Configure EF Core model for Issue entity in AppDbContext
    Added OnModelCreating method to define composite primary key,
    indexes, and unique constraints for the Issue entity.
    Configured relationships between Issue and User, including
    foreign key and delete behavior. Enhancements improve
    database integrity and query performance.

--- MaintenancePortal/Data/AppDbContext.cs
+++ MaintenancePortal/Data/AppDbContext.cs
@@ -7,5 +7,32 @@ namespace MaintenancePortal.Data;
 public class AppDbContext(DbContextOptions<AppDbContext> options)
      : IdentityDbContext<User>(options)
 {
-    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Composite primary key on ParentGene and SelfGene
+        modelBuilder.Entity<Issue>()
+            .HasKey(i => new { i.ParentGene, i.SelfGene });
+
+        // Index on ParentGene for faster lookups
+        modelBuilder.Entity<Issue>()
+            .HasIndex(i => i.ParentGene);
+
+        // Index on SelfGene for faster lookups
+        modelBuilder.Entity<Issue>()
+            .HasIndex(i => i.SelfGene);
+
+        // Unique constraint on Title and CreatedByUserId
+        modelBuilder.Entity<Issue>()
+            .HasIndex(i => new { i.Title, i.CreatedByUserId })
+            .IsUnique();
+
+        // Configure relationship between Issue and User (CreatedByUser)
+        modelBuilder.Entity<Issue>()
+            .HasOne(i => i.CreatedByUser)
+            .WithMany()
+            .HasForeignKey(i => i.CreatedByUserId)
+            .OnDelete(DeleteBehavior.Restrict); // Avoid cascade delete on users
+    }
 }
