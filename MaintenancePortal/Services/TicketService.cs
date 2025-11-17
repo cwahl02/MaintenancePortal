6b6b1a1057f0e2207f9acd4105065ff7d075df07
CW-ISM <cwismgitism@gmail.com>
2025-11-16 21:47:08 -0800

    Add TicketService with basic CRUD operations
    Introduced a new `TicketService` class implementing the `ITicketService` interface to manage ticket-related operations. Added methods for creating, retrieving, and updating tickets using a `DataAccessor` repository. Included necessary `using` directives for dependencies. Commented out unimplemented methods (`GetTicketByIdAsync`, `DeleteTicketAsync`, `SearchTicketsAsync`) as placeholders for future functionality.

---
+++ MaintenancePortal/Services/TicketService.cs
@@ -0,0 +1,24 @@
+﻿using MaintenancePortal.Data;
+using MaintenancePortal.Interfaces;
+using MaintenancePortal.Models;
+using MaintenancePortal.Repository;
+using System.Linq.Expressions;
+
+namespace MaintenancePortal.Services;
+
+public class TicketService : ITicketService
+{
+    private readonly DataAccessor _repo;
+
+    public TicketService(IDataAccessor repo)
+    {
+        _repo = (DataAccessor?)repo!;
+    }
+
+    public Task<Ticket?> CreateTicketAsync(Ticket ticket) => _repo.CreateAsync<Ticket>(ticket);
+    //public Task<Ticket?> GetTicketByIdAsync(ushort id);
+    public Task<IEnumerable<Ticket>> GetAllTicketsAsync() => _repo.GetAllAsync<Ticket>();
+    public Task<Ticket?> UpdateTicketAsync(ushort id, Ticket updatedTicket) => _repo.UpdateAsync<Ticket>(updatedTicket);
+    //Task<bool> DeleteTicketAsync(ushort id) => _repo.DeleteAsync<Ticket>(id);
+    //Task<IEnumerable<Ticket>> SerachTicketsAsync(Expression<Func<bool, Ticket>> predicate) => new List<Ticket>();
+}
