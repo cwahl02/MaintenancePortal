f51876abbc1625a1fe51be1da412258995c8c395
CW-ISM <cwismgitism@gmail.com>
2025-11-16 21:47:58 -0800

    Add ITicketService interface with core ticket methods
    Introduced the `ITicketService` interface under the `MaintenancePortal.Interfaces` namespace.
    
    - Added methods for creating, retrieving, and updating tickets:
      - `CreateTicketAsync`
      - `GetAllTicketsAsync`
      - `UpdateTicketAsync`
    - Commented out methods for ticket retrieval by ID, deletion, and searching:
      - `GetTicketByIdAsync`
      - `DeleteTicketAsync`
      - `SearchTicketsAsync`
    
    Included necessary `using` directives for `MaintenancePortal.Models` and `System.Linq.Expressions`.

---
+++ MaintenancePortal/Interfaces/ITicketService.cs
@@ -0,0 +1,14 @@
+﻿using MaintenancePortal.Models;
+using System.Linq.Expressions;
+
+namespace MaintenancePortal.Interfaces;
+
+public interface ITicketService
+{
+    Task<Ticket?> CreateTicketAsync(Ticket ticket);
+    //Task<Ticket?> GetTicketByIdAsync(ushort id);
+    Task<IEnumerable<Ticket>> GetAllTicketsAsync();
+    Task<Ticket?> UpdateTicketAsync(ushort id, Ticket updatedTicket);
+    //Task<bool> DeleteTicketAsync(ushort id);
+    //Task<IEnumerable<Ticket>> SerachTicketsAsync(Expression<Func<bool, Ticket>> predicate);
+}
