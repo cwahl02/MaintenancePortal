69d1e0a6be81fcfdb8999c054af6b17f79db6e9f
CW-ISM <cwismgitism@gmail.com>
2025-11-16 21:47:25 -0800

    Add TicketsController with sample data and views
    Introduced a new `TicketsController` class to manage ticket-related actions.
    Added `Index` action to display a list of hardcoded tickets and a `New`
    action to render a view for creating new tickets. Included necessary
    `using` directives and set up a constructor for dependency injection,
    though the `_repo` field is not yet utilized.

---
+++ MaintenancePortal/Controllers/TicketsController.cs
@@ -0,0 +1,53 @@
+﻿using MaintenancePortal.Models;
+using MaintenancePortal.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MaintenancePortal.Controllers;
+
+public class TicketsController : Controller
+{
+    private readonly DataAccessor _repo;
+
+    public TicketsController(IDataAccessor factory)
+    {
+    }
+    public IActionResult Index()
+    {
+        var user = new User
+        {
+            FirstName = "John",
+            LastName = "Doe",
+            BirthDate = new DateTime(1990, 1, 1),
+            UserName = "johndoe"
+        };
+
+        var tickets = new List<Ticket>
+        {
+            new Ticket
+            {
+                Title = "Leaky Faucet",
+                Description = "The kitchen faucet is leaking.",
+                CreatedBy = user,
+                CreatedById = user.Id,
+                CreatedAt = DateTime.Now.AddDays(-2),
+                Status = true
+            },
+            new Ticket
+            {
+                Title = "Broken Window",
+                Description = "The living room window is broken.",
+                CreatedBy = user,
+                CreatedById = user.Id,
+                CreatedAt = DateTime.Now.AddDays(-1),
+                Status = true
+            }
+        };
+
+        return View(tickets);
+    }
+
+    public IActionResult New()
+    {
+        return View();
+    }
+}
