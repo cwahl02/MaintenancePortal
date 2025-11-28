8e34b618e28def47f53f51ed78561ea8aeb55542
CW-ISM <cwismgitism@gmail.com>
2025-11-28 11:49:05 -0800

    Add TicketController and UserController with actions
    Added a new `TicketController` with an `Index` action to handle
    ticket-related views. Introduced a `UserController` with `Login`
    and `Register` actions, each supporting both `HttpGet` and
    `HttpPost` methods. The `Login` action includes placeholder
    logic for user authentication, while the `Register` action
    includes placeholder logic for user registration.
    
    Included necessary `using` directives for `Microsoft.AspNetCore.Mvc`
    in both controllers and `MaintenancePortal.Models` in `UserController`.

---
+++ MaintenancePortal/Controllers/TicketController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MaintenancePortal.Controllers;
+
+public class TicketController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ MaintenancePortal/Controllers/UserController.cs
@@ -0,0 +1,57 @@
+﻿using MaintenancePortal.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MaintenancePortal.Controllers;
+
+public class UserController : Controller
+{
+    [HttpGet]
+    public IActionResult Login(string? returnUrl = null)
+    {
+        ViewData["ReturnUrl"] = returnUrl;
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginViewModel model, string? returnUrl = null)
+    {
+        if(!ModelState.IsValid)
+        {
+            return View(model);
+        }
+
+        // TODO: Login manager
+        var result = true;
+
+        if (result)
+        {
+            return RedirectToAction("Index", "Ticket");
+        }
+
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Register(RegisterViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(model);
+        }
+
+        var result = true;
+
+        if (result)
+        {
+            return RedirectToAction("Login", "User");
+        }
+
+        return View();
+    }
+}
