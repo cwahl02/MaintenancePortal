38f5b1dc0efbd01ab95a60e4a40e87583b706198
CW-ISM <cwismgitism@gmail.com>
2025-11-28 13:44:50 -0800

    Add UserPermission class to manage user permissions
    A new `UserPermission` class was introduced in the `MaintenancePortal.Models` namespace. This class includes properties to represent user permissions, such as `UserId`, `PermissionId`, `AcquisitionType`, `SourceId`, and `AcquiredOn`. These properties enable tracking and management of user permissions within the system.

---
+++ MaintenancePortal/Models/UserPermission.cs
@@ -0,0 +1,10 @@
+﻿namespace MaintenancePortal.Models;
+
+public class UserPermission
+{
+    public string UserId { get; set; }
+    public int PermissionId { get; set; }
+    public string AcquisitionType { get; set; }
+    public string SourceId { get; set; }
+    public DateTime AcquiredOn { get; set; }
+}
