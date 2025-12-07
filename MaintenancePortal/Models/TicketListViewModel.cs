8cdebbe62a7b3c47e88437da224e80121e3406e2
CW-ISM <cwismgitism@gmail.com>
2025-12-06 22:27:50 -0800

    Add Query property to TicketListViewModel
    A new `Query` property of type `string` was added to the
    `TicketListViewModel` class. This property is likely intended
    to store a search or filter query related to the tickets.

--- MaintenancePortal/Models/TicketListViewModel.cs
+++ MaintenancePortal/Models/TicketListViewModel.cs
@@ -2,6 +2,7 @@
 
 public class TicketListViewModel
 {
+    public string Query;
     public PaginationMetadata PaginationMetadata { get; set; } = new PaginationMetadata();
     public IEnumerable<int> Pagination { get; set; } = new List<int>();
     public IEnumerable<Ticket> Tickets { get; set; } = new List<Ticket>();
