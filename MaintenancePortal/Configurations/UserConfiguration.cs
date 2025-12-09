07fb3eea8c55fd82d2addb3968a876e539ed81d8
CW-ISM <cwismgitism@gmail.com>
2025-12-08 21:04:27 -0800

    Add UserConfiguration for EF Core entity setup
    Added UserConfiguration.cs implementing IEntityTypeConfiguration<User> with an empty Configure method. This prepares for future custom configuration of the User entity in Entity Framework Core. Required using directives for models and EF Core are included.

---
+++ MaintenancePortal/Configurations/UserConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using MaintenancePortal.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MaintenancePortal.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder) { }
+}
