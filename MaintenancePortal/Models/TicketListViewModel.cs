11a95825f391107d11144cfeb32b9dcf52598fd2
CW-ISM <cwismgitism@gmail.com>
2025-12-11 18:56:13 -0800

    Remove obsolete ticket view model classes
    Deleted TicketEditableViewModel, TicketListViewModel, TicketModifyViewModel, and TicketViewModel from the MaintenancePortal.Models namespace, along with all their properties and constructors, as they are no longer needed in the codebase.

--- MaintenancePortal/Models/TicketListViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MaintenancePortal.Models;
-
-public class TicketListViewModel
-{
-    public string Query;
-    public PaginationMetadata PaginationMetadata { get; set; } = new PaginationMetadata();
-    public IEnumerable<int> Pagination { get; set; } = new List<int>();
-    public IEnumerable<TicketIndexViewModel> Tickets { get; set; } = new List<TicketIndexViewModel>();
-}
