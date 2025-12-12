c22dbc7aec8aee1f705cfaa7144fc729de8c8a94
CW-ISM <cwismgitism@gmail.com>
2025-12-11 18:56:26 -0800

    Refactor ticket state to use IsOpen boolean property
    Removed the TicketState enum and State property from the Ticket
    class. Added a required IsOpen boolean property to indicate
    ticket status. All other Ticket class properties remain unchanged.

--- MaintenancePortal/Models/Ticket.cs
+++ MaintenancePortal/Models/Ticket.cs
@@ -3,17 +3,13 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MaintenancePortal.Models;
 
-public enum TicketState
-{
-    Open,
-    InProgress,
-    Closed
-}
-
 public class Ticket
 {
     [Key]
     public int Id { get; set; }
+
+    [Required]
+    public bool IsOpen { get; set; }
 
     [Required, StringLength(255)]
     public required string Title { get; set; }
@@ -30,8 +26,6 @@ public class Ticket
     [Required, Column(TypeName = "datetime2")]
     public DateTime LastModifiedAt { get; set; }
 
-    [Required, Column(TypeName = "nvarchar(32)")]
-    public TicketState State { get; set; }
 
     [Required]
     public required string CreatedById { get; set; }
